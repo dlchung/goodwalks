78ce47e9f1b7646a7d82a6f3a46253ab5e02ead2
Dan Chung <danetix@gmail.com>
2016-11-28 19:17:11 -0500

    implmented material-ui into autosuggest

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { MuiThemeProvider } from 'material-ui';
 import { moodColor } from './components/mood_color';
 
 export default class App extends Component {
   render() {
     moodColor(JSON.parse(localStorage.getItem('mood')));
     return (
-      <div>
-        <div className="nav">
-          <Link to="/">Home</Link>
-          <Link to="/settings">Settings</Link>
+      <MuiThemeProvider>
+        <div>
+          <div className="nav">
+            <Link to="/">Home</Link>
+            <Link to="/settings">Settings</Link>
+          </div>
+          {this.props.children}
         </div>
-        {this.props.children}
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
