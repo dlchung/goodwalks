9347aebb31db3515fe3f3cb401237bd90351a703
Dan Chung <danetix@gmail.com>
2016-11-30 13:45:25 -0500

    refactored form, added callback to update parent state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,49 +1,25 @@
 import React, { Component } from 'react';
 import Geosuggest from 'react-geosuggest-material-ui';
-import FlatButton from 'material-ui/FlatButton';
+
 
 export default class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      locationLabel: JSON.parse(localStorage.getItem('locationLabel')),
-      coordinates: '',
-      textHelp: ''
-    };
-
-    this.onFormSubmit = this.onFormSubmit.bind(this);
     this.onSuggestSelect = this.onSuggestSelect.bind(this);
   }
 
   onSuggestSelect(event) {
-    //console.log(event);
-    this.setState({ locationLabel: event.label });
-    this.setState({ coordinates: event.location });
-  }
-
-  onFormSubmit(event) {
-    // Stop default form behavior
-    event.preventDefault();
-
-    // Save city name and city coordinates to localStorage
-    localStorage.setItem('locationLabel', JSON.stringify(this.state.locationLabel));
-    localStorage.setItem('coordinates', JSON.stringify(this.state.coordinates));
-    this.setState({ textHelp: "Your settings have been saved." });
+    this.props.onUserInput(event.label, event.location);
   }
 
   render() {
     return (
-      <form onSubmit={this.onFormSubmit}>
-        <label htmlFor="location-input">Your Location:</label>
         <Geosuggest
           placeholder="Enter your location"
           types={['(regions)']}
           onSuggestSelect={this.onSuggestSelect}
         />
-        <div className="text-help">{this.state.textHelp}</div>
-        <FlatButton label="Save Settings" type="submit" />
-      </form>
     );
   }
 }
--- src/components/settings.js
+++ src/components/settings.js
@@ -1,12 +1,57 @@
 import React, { Component } from 'react';
 import SearchBar from './search_bar';
+import FlatButton from 'material-ui/FlatButton';
 
 export default class Settings extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      locationLabel: JSON.parse(localStorage.getItem('locationLabel')),
+      coordinates: '',
+      textHelp: ''
+    };
+
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.handleSearchBarChange = this.handleSearchBarChange.bind(this);
+  }
+
+  onFormSubmit(event) {
+    // Stop default form behavior
+    event.preventDefault();
+
+    // Save city name and city coordinates to localStorage
+    localStorage.setItem('locationLabel', JSON.stringify(this.state.locationLabel));
+    localStorage.setItem('coordinates', JSON.stringify(this.state.coordinates));
+    this.setState({ textHelp: "Your settings have been saved." });
+  }
+
+  handleSearchBarChange(locationLabel, coordinates) {
+    this.setState({
+      locationLabel,
+      coordinates
+    });
+  }
+
   render() {
     return(
       <div className="settings-wrap">
         <h1>Settings</h1>
-        <SearchBar />
+        <form onSubmit={this.onFormSubmit}>
+          <label htmlFor="location-input">Your Location:</label>
+
+          <SearchBar
+            onUserInput={this.handleSearchBarChange}
+          />
+          <div className="text-help">{this.state.textHelp}</div>
+
+          <FlatButton
+            label="Save Settings"
+            type="submit"
+            backgroundColor="#ccc"
+            hoverColor="#eee"
+          />
+        </form>
       </div>
     );
   }
